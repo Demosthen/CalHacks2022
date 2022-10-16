54631b29200a06dc77091585ad96b36c7ace3b30
ywangbk2023 <70738865+ywangbk2023@users.noreply.github.com>
2022-10-16 12:04:16 -0700

    Working Template! Song switch between between two songs

--- Moodsic_1.0/app.js
+++ Moodsic_1.0/app.js
@@ -1,3 +1,4 @@
+hmApp.setScreenKeep(true)
 var logger = DeviceRuntimeCore.HmLogger.getLogger("HelloWorld")
 var stress = null
 var music = null
@@ -7,10 +8,11 @@ var last_heart_rate = -64543
 App({
   globalData: {},
   onCreate(options) {
+    
     stress = hmSensor.createSensor(hmSensor.id.HEART)
     music = hmSensor.createSensor(hmSensor.id.MUSIC)
     const hrCurrListener = function () {
-      console.log(hrSensor.last)
+      console.log(stress.last)
     }
     stress.addEventListener(stress.event.CURRENT, hrCurrListener)
     music.audInit()
@@ -22,7 +24,7 @@ App({
     const interval = setInterval(function() {
       const value = stress.last
       console.log(value)
-      logger.log(value)
+      logger.log("Current song title is ", current_title)
       if (current_title !== null) {
         if (music.title !== current_title) {
           is_happy_music = !is_happy_music
@@ -31,15 +33,21 @@ App({
 
       current_title = music.title
       console.log(typeof value)
-      if (value > 95 ) {
+      if (value > 5 ) {
         // high stress, divert
         if (!is_happy_music) {
+          logger.log("Song change triggered!", is_happy_music)
           music.audNext()
         }
       }
 
       last_heart_rate = value
     }, 2000)
+    const interval2 = setInterval(function() {
+      logger.log("MUAHAHAHAHAHAHAHAHA I changed ur songgggg")
+      music.audNext()
+      
+    }, 30000)
   },
 
 
