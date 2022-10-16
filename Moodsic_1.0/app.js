762f4c715143fd9117c9b63b1af536f1464a024d
ywangbk2023 <70738865+ywangbk2023@users.noreply.github.com>
2022-10-16 11:12:01 -0700

    semi kind of not working

--- Moodsic_1.0/app.js
+++ Moodsic_1.0/app.js
@@ -1,28 +1,51 @@
+var logger = DeviceRuntimeCore.HmLogger.getLogger("HelloWorld")
 var stress = null
 var music = null
+var is_happy_music = false
+var current_title = null
+var last_heart_rate = -64543
 App({
   globalData: {},
   onCreate(options) {
-    const stress_ = hmSensor.createSensor(hmSensor.id.STRESS)
-    const music_ = hmSensor.createSensor(hmSensor.id.MUSIC)
-    stress = stress_
-    music = music_
+    stress = hmSensor.createSensor(hmSensor.id.HEART)
+    music = hmSensor.createSensor(hmSensor.id.MUSIC)
+    const hrCurrListener = function () {
+      console.log(hrSensor.last)
+    }
+    stress.addEventListener(stress.event.CURRENT, hrCurrListener)
     music.audInit()
     music.audPlay()
     console.log(stress.current)
-    console.log(stress.time)
+    // console.log(stress.time)
     console.log('app on create invoke')
-
+  
     const interval = setInterval(function() {
-      const value = stress.current
+      const value = stress.last
       console.log(value)
-    }, 5000)
-    const interval2 = setInterval(function() {
-      music.audPause()
-    }, 5000)
+      logger.log(value)
+      if (current_title !== null) {
+        if (music.title !== current_title) {
+          is_happy_music = !is_happy_music
+        }
+      }
+
+      current_title = music.title
+      console.log(typeof value)
+      if (value > 95 ) {
+        // high stress, divert
+        if (!is_happy_music) {
+          music.audNext()
+        }
+      }
+
+      last_heart_rate = value
+    }, 2000)
   },
 
+
+  
   onDestroy(options) {
     console.log('app on destroy invoke')
+    stress.removeEventListener(stress.event.CURRENT, hrCurrListener)
   }
 })
