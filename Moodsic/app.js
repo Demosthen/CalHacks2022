8cfba27d0035a38efb0e265bd524a51b56015749
Adam Chois <dragonboy174@gmail.com>
2022-10-16 01:19:27 -0700

    added proper permissions and got stress to interval

--- Moodsic/app.js
+++ Moodsic/app.js
@@ -1,10 +1,24 @@
+// import {hmSensor} from "hmSensor";
+import {Stress} from '@zos/sensor';
+var stress = null;
 App({
   globalData: {},
   onCreate(options) {
     console.log('app on create invoke')
+    const stress_ = new Stress() //Stress.createSensor(hmSensor.id.STRESS)
+    stress = stress_
+    const {value} = stress.getCurrent()
+    console.log(value)
+    console.log("aaaaaaaaa")
+
+    const interval = setInterval(function() {
+      const {value} = stress.getCurrent()
+      console.log(value)
+    }, 5000)
   },
 
   onDestroy(options) {
     console.log('app on destroy invoke')
   }
 })
+ 
